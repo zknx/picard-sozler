7c74a321098b429a42ffe908ad5efa7fb21c8549
zknx <zknx.dev@gmail.com>
2026-09-09 21:00:06 +0000

    fix errors

--- __init__.py
+++ __init__.py
@@ -7,33 +7,31 @@
 #
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+from functools import partial
 
 from picard.plugin3.api import PluginApi
-
-from functools import partial
-import json
+from picard.track import Track
 
 
-def log_debug(s):
+def log_debug(api: PluginApi, s):
     api.logger.debug(f"{'Picard Sözler'}: {s}")
 
 
-def log_err(s):
+def log_err(api: PluginApi, s):
     api.logger.error(f"{'Picard Sözler'}: {s}")
 
 
-def process_response(album, metadata, data, reply, error):
+def process_response(api: PluginApi, track: Track, metadata, data, reply,
+                     error):
     if error:
-        album._requests -= 1
-        album._finalize_loading(None)
         return
 
     try:
-        log_debug("starting to process")
-        log_debug(f"got response: {data}")
+        log_debug(api, "starting to process")
+        log_debug(api, f"got response: {data}")
         instrumental = data.get("instrumental")
         if instrumental:
-            log_debug("instrumental track; skipping")
+            log_debug(api, "instrumental track; skipping")
             lyrics = None
         else:
             # Fallbacks to plain, ie, unsynced lyrics.
@@ -42,13 +40,10 @@ def process_response(album, metadata, data, reply, error):
         if lyrics is not None:
             metadata["lyrics"] = lyrics
     except AttributeError:
-        log_err(f"api malformed response: {data}")
-    finally:
-        album._requests -= 1
-        album._finalize_loading(None)
+        log_err(api, f"api malformed response: {data}")
 
 
-def process_track(album, metadata, track, __):
+def process_track(api: PluginApi, track: Track, metadata, _, __):
     (mins, secs) = map(int, metadata["~length"].split(":"))
     query = {
         "artist_name": metadata["albumartist"] or metadata["artist"],
@@ -56,14 +51,13 @@ def process_track(album, metadata, track, __):
         "track_name": metadata["title"],
         "duration": mins * 60 + secs,  # accepts seconds only
     }
-    log_debug(f"trying to query with: {query}")
-    album.tagger.webservice.get_url(
+    log_debug(api, f"trying to query with: {query}")
+    track.tagger.webservice.get_url(
         url="https://lrclib.net/api/get",
-        handler=partial(process_response, album, metadata),
+        handler=partial(process_response, api, track, metadata),
         parse_response_type="json",
         queryargs=query,
     )
-    album._requests += 1
 
 
 def enable(api: PluginApi):
